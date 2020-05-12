0f837006ea66062a61506aa8e200666af6865bd2
Johan Steenhoven <johan.steenhoven@nn-group.com>
2020-05-12 20:37:43 +0200

    Build directory list

--- main.go
+++ main.go
@@ -1,31 +1,44 @@
 package main
 
-// {
-//     "files": ["http://192.168.178.7:2480/Super%20Mario%2064%20%28Unofficial%20Native%20Homebrew%20Port%20for%20Nintendo%20Switch%20by%20AtlasNX%29%20%5BNSP%5D/Super%20Mario%2064%20%28Unofficial%20Native%20Homebrew%20Port%20for%20Nintendo%20Switch%20by%20AtlasNX%29%20%5B054507E0B7552000%5D%5Bv0%5D.nsp"],
-//     "directories": []
-// }
-
 import (
-	"encoding/json"
+	// "log"
 	"fmt"
 	"net/url"
+	"os"
+	"path/filepath"
 )
 
-type nspindex struct {
-	Files       []string
-	Directories []string
+func WalkMatch(root, pattern string) ([]string, error) {
+	var matches []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if matched, err := filepath.Match(pattern, filepath.Base(path)); err != nil {
+			return err
+		} else if matched {
+			matches = append(matches, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return matches, nil
 }
 
 func main() {
+	files, _ := WalkMatch("/usr/src/", "*.go")
+	fmt.Println(files)
+
 	webpage := "http://192.168.178.7:2480/"
-	nsp := "Super Mario 64 (Unofficial Native Homebrew Port for Nintendo Switch by AtlasNX) [054507E0B7552000][v0].nsp"
 
-	var directories []string
-	output := &nspindex{
-		Files:       []string{webpage + url.QueryEscape(nsp)},
-		Directories: directories,
+	for i, s := range files {
+		files[i] = webpage + url.QueryEscape(s)
+		fmt.Println(files)
+
 	}
-	outputJ, _ := json.Marshal(output)
-	fmt.Println(string(outputJ))
-
 }
